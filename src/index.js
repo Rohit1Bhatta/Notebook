9d6cf2bae982e860b1124b09f4956c69f9e08b33
Rohit <bhattarohit7@gmail.com>
2023-01-19 17:02:18 +0545

    remove by winnee

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-// import "asdfasdf";
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
